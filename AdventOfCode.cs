878b30bbea2327713a251957037d6e95e9825222
ctafield <chris@ctafield.com>
2019-12-01 20:11:22 +0000

    Day 1

---
+++ AdventOfCode.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+public class AdventOfCode
+{
+
+    private int[] Masses { get; set; }
+
+    public AdventOfCode(IEnumerable<string> input)
+    {
+        Masses = input.Select(x => int.Parse(x)).ToArray();
+    }
+
+    public int GetPart1()
+    {
+        return Masses.Select(x => (int)(x / 3) - 2).Sum();
+    }
+
+    private int GetFuel(int mass) {
+        var x = (mass / 3) - 2;
+        if (x <= 0) {
+            return 0;
+        }
+
+        return x + GetFuel(x);
+    }
+
+    public long GetPart2()
+    {
+        return GetDay1() + Masses.Select(x => (int)(x / 3) - 2).Select(x => GetFuel(x)).Sum();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace adventofcode2019
+{
+    class Program
+    {
+        private static string[] LoadInput()
+        {
+            return System.IO.File.ReadAllLines("input.txt");
+        }
+
+        static void Main(string[] args)
+        {
+            var input = LoadInput();
+            var adventOfCode = new AdventOfCode(input);
+        
+            Console.WriteLine($"1. {adventOfCode.GetPart1()}");
+            Console.WriteLine($"2. {adventOfCode.GetPart2()}");
+        }
+    }
+}
